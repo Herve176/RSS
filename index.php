a77bd2a7009f89e7cc1ec7a523e57464c8e7444d
Herve176 <78292511+Herve176@users.noreply.github.com>
2023-06-07 21:31:36 +0200

    Update index.php
    added array and remove form

--- index.php
+++ index.php
@@ -1,54 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+for ($i = 0; $i < 4; $i++) {
+    $rss = array("https://www1.cbn.com/rss-cbn-articles-cbnnews.xml", "https://www1.cbn.com/rss-cbn-blogs-thebrodyfile.xml", "https://www1.cbn.com/rss-cbn-blogs-jerusalemdateline.xml", "https://www1.cbn.com/rss-cbn-blogs-hurdontheweb.xml");
+    $rss = $rss[$i];
+    $xml = simplexml_load_file($rss);
+    $items = $xml->channel->item;
+    echo '<ul>';
+    foreach ($items as $item) {
+        $title = $item->title;
+        $description = $item->description;
+        $link = $item->link;
+        echo '<li>';
+        echo '<h3><a href="' . $link . '">' . $title . '</a></h3>';
+        echo '<p>' . $description . '</p>';
+        echo '</li>';
+    }
+    echo '</ul>';
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>RSS</title>
-</head>
-
-<body>
-    <div>
-        <form method="post" action="">
-            <input name="rss" type="text" placeholder="Enter RSS URL">
-            <input type="submit"><br>
-
-            <?php
-            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                $rss = $_POST['rss'];
-                $xml = simplexml_load_file($rss);
-                $items = $xml->channel->item;
-                echo '<ul>';
-                foreach ($items as $item) {
-                    $title = $item->title;
-                    $description = $item->description;
-                    $link = $item->link;
-                    echo '<li>';
-                    echo '<h3><a href="' . $link . '">' . $title . '</a></h3>';
-                    echo '<p>' . $description . '</p>';
-                    echo '</li>';
-                }
-                echo '</ul>';
-
-                // Save RSS data as JSON file
-                $data = [];
-                foreach ($items as $item) {
-                    $data[] = [
-                        'title' => (string) $item->title,
-                        'description' => (string) $item->description,
-                        'link' => (string) $item->link,
-                    ];
-                }
-                $jsonData = json_encode($data, JSON_PRETTY_PRINT);
-                $file = 'rss_data.json';
-                file_put_contents($file, $jsonData);
-                echo 'RSS data saved as JSON in ' . $file;
-            }
-            ?>
-
-        </form>
-    </div>
-</body>
-
-</html>
+    // Save RSS data as JSON file
+    $data = [];
+    foreach ($items as $item) {
+        $data[] = [
+            'title' => (string) $item->title,
+            'description' => (string) $item->description,
+            'link' => (string) $item->link,
+        ];
+    }
+    $jsonData = json_encode($data, JSON_PRETTY_PRINT);
+    $file = "rss_data{$i}.json";
+    file_put_contents($file, $jsonData);
+    echo 'RSS data saved as JSON in ' . $file;
+}
